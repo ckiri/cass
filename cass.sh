f713366cda3cb174cbe462af7889c10869af6ce3
Chris Kiriakou <ckiriakou@stud.hs-heilbronn.de>
2023-02-23 10:28:28 +0100

    Added VM setup

--- cass.sh
+++ cass.sh
@@ -1,45 +1,52 @@
 #!/bin/sh
 # Update System
-pacman -Syu sudo 
+pacman -Syy                                                                     # update package index
+pacman -S archlinux-keyring                                                     # update keyring
+pacman -Syu sudo                                                                # update the whole system
 
-echo "Following are a few questions for setting up the system\n"
-
+# Questions for installing the right SW
+echo ""
+echo "*******************************************************"
+echo "Following are a few questions for setting up the system"
+echo ""
 echo "Enter Username for user creation: "
 read uid
-echo "\n"
-
-echo "Are you using a mobile device or desktop?\n"
+echo ""
+echo "Are you using a mobile device or desktop?"
 echo "1 = mobile, 2 = desktop: "
 read device
-echo "\n"
-
-echo "Do you want to setup dualboot?\n"
+echo ""
+echo "Do you want to setup dualboot?"
 echo "[y/N]: "
 read boot
-echo "\n"
-
-echo "Do you want to install a vpn?\n"
+echo ""
+echo "Do you want to install a vpn?"
 echo "[y/N]: "
 read vpn
-echo "\n"
-
+echo ""
 echo "Do you want to change the shell to zsh?"
-echo "[y/N]. "
+echo "[y/N]: "
 read shell
-echo "\n"
+echo ""
+echo "Do you want to install and setup a vm?"
+echo "[y/N]: "
+read vm
+echo "*******************************************************"
+echo ""
 
 # Create a user
 useradd -m $uid                                                                 # create user with a homedir
 passwd $uid                                                                     # give the user a pw
-usermod -aG wheel,power,storage,audio,video,optical $uid                        # add user to different groups
+usergroups='wheel,power,storage,audio,video,optical'
+usermod -aG $usergroups $uid                                                    # add user to different groups
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers    # give wheel group sudo privleges
 
 # Setup dualboot
 if [ $boot == 'y' ]
 then
     pacman -S os-prober ntfs-3g
-    sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/'\n
-    /etc/default/grub
+    dualbootcfg='s/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/'
+    sed -i $dualbootcfg /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
@@ -61,7 +68,7 @@ sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8' /etc/locale.gen
 locale-gen
 
 # Install SW from list
-pacman -S $(awk -F ',' '{print $1}' sw.csv | awk 'NR!=1 {print}')              # print package name as argument for pacman
+pacman -S $(awk -F ',' '{print $1}' sw.csv | awk 'NR!=1 {print}')               # read first column of SW list, pipe into awk & print rest without frist line
 
 # Change Shell to zsh
 if [ $shell == 'y' ]
@@ -88,12 +95,19 @@ then
     sudo pacman -S tlp python-iwlib                                             # Power Management
 fi
 
+# Install and setup QEMU/KVM
+if [ $vm == 'y' ]
+then
+    pacman -Syu qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils
+                openbsd-netcat ebtables iptables libgeustfs
+    usermod -aG libvirt $uid
+    systemctl enable libvirtd.service                                               # Enable Virtualisation for Virtualbox
+    systemctl start libvirtd.service
+fi
+
 # Start services
 systemctl enable cups.service                                                   # Enable Printing Server (Start Service on every startup)
 systemctl start cups.service                                                    # Start Printing Server now
-
-systemctl enable libvirtd.service                                               # Enable Virtualisation for Virtualbox
-systemctl start libvirtd.service
 
 systemctl enable ntpd.service
 systemctl start ntpd.service
@@ -115,4 +129,4 @@ then
 fi
 
 echo "Setup Finished, exiting now...\n"
-exit
+exit 0
